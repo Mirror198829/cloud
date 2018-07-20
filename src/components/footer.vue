e94f784461115ba596e55aae05fa40701ae84163
Mirror198829 <412366756@qq.com>
2018-07-20 11:50:27 +0800

    完成footer 中间模块页面

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -16,13 +16,31 @@
         </li>
       </ul>
       <div class="footerMenu clearfix">
-        <ul class="footerNavMenu">
+        <ul class="footerNavMenu  clearfix">
           <li class="navItem" v-for="(item,key) in navLst">
             <h3>{{item.title}}</h3>
             <p v-for="(nav,index) in item.subNavs">{{nav}}</p>
           </li>
         </ul>
-        <div class="footerSubMenu"></div>
+        <ul class="footerSubMenu clearfix">
+          <li class="subNavItem clearfix">
+            <h3>关注阿里云</h3>
+            <div class="subNavItemO">
+              <p>阿里云微信</p>
+              <img src="../assets/home/id.png" height="70" width="70" style="margin-top:5px;"/>
+            </div>
+            <div class="subNavItemO">
+              <p>阿里云微博</p>
+              <img src="../assets/home/id.png" height="70" width="70" style="margin-top:5px;"/>
+            </div>
+          </li>
+          <li class="subNavItem">
+            <h4 v-for="(item,key) in subNavLst" class="subNavRightItem">
+              <i class="fa subNavIcon" :class="item.icon"></i>
+              {{item.title}}
+            </h4>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -47,6 +65,11 @@ export default {
         {title:'快速链接',subNavs:['免费注册','管理控制台','备案中心','我的订单','费用中心','价格计算器','图标库']},
         {title:'其他链接',subNavs:['阿里官网','企业业务','开发者联盟','阿里招聘','开发者中心','阿里商城','关于我们','联系我们']},
         {title:'联系我们',subNavs:['售前邮箱','备案服务邮箱','云市场咨询邮箱']}
+      ],
+      subNavLst:[
+        {icon:'fa-headphones',title:'4000-955-988'},
+        {icon:'fa-universal-access',title:'华为云安全'},
+        {icon:'fa-support',title:'可信合规'}
       ]
     }
   },
@@ -60,6 +83,7 @@ export default {
 
 <style  scoped lang="less">
 @import '../less/index.less';
+@foot-color:#c3c5c6;
 .footerInfo{background-color:@base-color;color:@font-white;padding:20px 0}
 .footerMain{max-width:@max-pcW;width:@max-pcW;margin:0 auto;}
 .footerService{width:100%;margin-bottom:50px;}
@@ -67,13 +91,20 @@ export default {
 .footerIcon{font-size:35px;font-weight:400;color:#c3c5c6;margin-right:10px;position:relative;top:-4px;}
 .footerTxt{display:inline-block;
   h2{font-size:16px;line-height:25px;font-weight:400;text-align:left;}
-  p{font-size:14px;color:#c3c5c6;text-align:left;}
+  p{font-size:14px;color:@foot-color;text-align:left;}
 }
 .footerNavMenu{width:calc(100% / 7 * 5);float:left;}
 .footerSubMenu{float:left;width:calc(100% / 7 * 2);}
 .navItem{float:left;width:calc(100% / 5);
   h3{font-size:14px;font-weight:400;line-height:20px;margin-bottom:16px;}
-  p{font-size:14px;color:#c3c5c6;padding:2px 0}
+  p{font-size:14px;color:@foot-color;padding:2px 0}
+}
+.subNavItem{float:left;width:calc(100% / 2);
+  h3{font-size:14px;font-weight:400;line-height:20px;margin-bottom:16px;}
+  p{font-size:14px;color:@foot-color;padding:2px 0}
+}
+.subNavRightItem{font-weight:400;font-size:16px;color:@foot-color;margin-bottom:20px;
+  .subNavIcon{font-size:25px;margin-right:7px;position:relative;top:2px;}
 }
 // screen >= 1200
 @media screen and (min-width:1200px){
@@ -89,13 +120,27 @@ export default {
     p{text-align:center;}
   }
   .footerNavMenu{width:calc(100%);float:none;}
-  .footerSubMenu{float:none;width:calc(100%);}
+  .footerSubMenu{float:none;width:410px;margin:20px auto}
+  .subNavItem{float:left;
+    h3{display:none;}
+    .subNavItemO{float: left;margin-left:20px;}
+    p{font-size:14px;color:@foot-color;padding:2px 0}
+  }
 }
 
 @media screen and (max-width:992px){
   .footerInfo{background-color:red}
-  .footerServicItem{width:calc(100% / 3);margin-bottom:40px;}
-
+  .footerServicItem{width:calc(100% / 3);margin-bottom:40px;
+    &:nth-child(n + 4){margin-bottom:0px;}
+  }
+  .footerService{margin-bottom:30px;}
+  .footerNavMenu{display:none;}
+  .footerSubMenu{width:290px;margin:0px auto}
+  .subNavItem{
+  .subNavItemO{float:none;margin-bottom:20px;
+      img{display:none;}
+    }
+  }
 }
 
 @media screen and (max-width:768px){
@@ -106,7 +151,13 @@ export default {
     h2{font-size:12px}
     p{font-size:12px;}
   }
-
+  .subNavItem{
+    p{font-size:12px;padding:2px 0}
+  }
+  .footerSubMenu{width:230px;}
+  .subNavRightItem{font-weight:400;font-size:12px;color:@foot-color;margin-bottom:20px;
+    .subNavIcon{font-size:14px;margin-right:7px;position:relative;top:2px;}
+  }
 }
 
 @media screen and (max-width:480px){
