18c0a9f5582c907522444584f941f6e02af9eb03
Mirror198829 <412366756@qq.com>
2018-08-02 09:17:51 +0800

    修改路径

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/cloud-exhibition/dist/',
+    assetsPublicPath: '/cloud/dist/',
 
     /**
      * Source Maps
