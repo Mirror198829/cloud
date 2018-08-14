45d36b7c7cf05229431c9b0aedf860fc57bdb6eb
Mirror198829 <412366756@qq.com>
2018-08-14 10:45:08 +0800

    '修改为响应式布局'

--- src/page/console/dashBoard.vue
+++ src/page/console/dashBoard.vue
@@ -1,6 +1,6 @@
 <!-- 
 - Author:CaoJing
-- Date:2018/8/13
+- Date:2018/8/14
 - github:https://github.com/Mirror198829
 -->
 <template>
@@ -43,7 +43,7 @@
       </div>
       <div class="dashBottom">
         <li v-for="(item,key) in 4" class="block">
-          
+          sadfasdf
         </li>
       </div>
     </main>
@@ -89,15 +89,15 @@ export default {
 #dashBoard{width:100%;min-height:100%;background-color:#f4f4f4}
 header{padding:20px;background-color:#fff;color:@font-theme;}
 main{padding:@boundary;}
-.dashTop{height:400px;margin-bottom: @boundary;display:flex;}
-.dashLeft{flex:2;margin-right: @boundary;display:flex;flex-direction:column;}
-.dashRight{flex:3;background-color:#fff;}
-.dashGauge{flex:2;margin-bottom:@boundary;display:flex}
+.dashTop{margin-bottom: @boundary;display:flex;}
+.dashLeft{flex:2;margin-right: @boundary;display:flex;flex-direction:column;height:400px;}
+.dashRight{flex:3;background-color:#fff;height:400px;}
+.dashGauge{flex:2;margin-bottom:@boundary;display:flex;}
 .dashGaugeItem{margin-right:@boundary;background-color:#fff;flex:1;}
 .dashGaugeItem:last-child{margin-right:0}
 .dashProgress{flex:1.8;background-color:#fff}
-.dashBottom{display:flex}
-.block{margin-right:@boundary;flex:1;height:350px;background-color:#fff;}
+.dashBottom{display:flex;}
+.block{margin-right:@boundary;height:350px;background-color:#fff;width:calc( ( 100% - @boundary * 3 ) / 4)}
 .block:last-child{margin-right:0;}
 .chartSection{display:flex;flex-direction:column;
   .chartTitle{font-size:12px;padding:10px;background-color:#fafafa;}
@@ -120,17 +120,35 @@ main{padding:@boundary;}
 }
 // 1200>= screen >=992
 @media screen and (max-width:1200px){
-  // .dashTop{display:block;}
-  // .dashLeft{width:100%;}
-  // .dashRight{width:100%}
+.dashTop{display:block;}
+.dashLeft{width:100%;margin-bottom:@boundary;}
+.dashRight{width:100%;}
 }
 
 @media screen and (max-width:992px){
+  .dashBottom{width:100%;flex-wrap:wrap;}
+  .block{margin-right:@boundary;width:calc( ( 100% - @boundary ) / 2);}
+  .block:nth-child(2n){margin-right:0}
+  .block:nth-child(1){margin-bottom:20px;}
+  .block:nth-child(2){margin-bottom:20px;}
 }
 
 @media screen and (max-width:768px){
+  .dashLeft{display:block;height:510px;}
+  .dashGauge{width:100%;display:block;}
+  .dashGaugeItem{margin-right:0;margin-bottom:@boundary;height:157px;}
+  .block{margin-right:0;width:100%;margin-bottom:20px;}
+  .block:last-child{margin-bottom:0;}
 }
-
 @media screen and (max-width:480px){
+  .dashProgress{padding:10px 5px;height:157px;
+  .progressItem{padding:5px;
+    .progressIcon{margin-right:3px;width:12px;font-size:12px;}
+    .progressTitle{margin-right:5px;font-size:12px;width:70px;}
+    .progress{height:7px;
+      .percent{font-size:12px;}
+    }
+  }
+}
 }
 </style>
